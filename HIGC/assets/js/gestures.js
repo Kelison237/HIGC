4ba7c65bc22cc795520ed06f28d9991dd05c5508
Kelison237 <77363856+Kelison237@users.noreply.github.com>
2022-07-21 21:41:41 +0100

    Add files via upload

--- HIGC/assets/js/gestures.js
+++ HIGC/assets/js/gestures.js
@@ -11,10 +11,15 @@ menu.addEventListener('click', ()=> {
     }else  if(document.querySelector('ul').style.marginTop = '-434px') {
         // document.querySelector('ul').style.marginTop = "-434px"
         document.querySelector('.menu').innerHTML = `<h2>Close</h2>`
-        document.querySelector('.purpleBg').style.height = `92vh`
+        // document.querySelector('.purpleBg').style.height = `92vh`
         document.querySelector('ul').style.marginTop = '0px';
         document.querySelector('ul').style.transition = '1s';
     
         console.log("evenebtter");
     }
 })
+
+// let menu = document.querySelector('.menu');
+// menu.addEventListener('click', () => {
+
+// })
